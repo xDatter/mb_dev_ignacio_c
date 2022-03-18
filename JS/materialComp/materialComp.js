04e1229482eb4c5f208b6f5b99fad9ce8b4f3dfd
icardenas <demwl_14@hotmail.com>
2022-03-18 17:43:17 -0300

    push js

--- JS/materialComp/materialComp.js
+++ JS/materialComp/materialComp.js
@@ -3,6 +3,7 @@ function toggleClass(){
     $("#toggleClass").toggleClass("off");
     $(".btn_back").toggleClass("off");
     $(".section1").toggleClass("off");
+    $(".section2").toggleClass("off");
     $(".MAqNLXLFR4WUfBoXHoCFQ.header-widget").toggleClass("off");
     $(".mbui-layout-header.layout-header").toggleClass("off");
 }
