cf9ee1d7198ef97eded156b9f0f1ce76b919343f
icardenas <demwl_14@hotmail.com>
2022-03-18 16:50:21 -0300

    wooo mucho tiempo

--- JS/materialComp/materialComp.js
+++ JS/materialComp/materialComp.js
@@ -1,8 +1,8 @@
 function toggleClass(){
     $(".fullcover").toggleClass("off");
     $("#toggleClass").toggleClass("off");
+    $(".btn_back").toggleClass("off");
     $(".section1").toggleClass("off");
-    $(".section1a").toggleClass("off");
     $(".MAqNLXLFR4WUfBoXHoCFQ.header-widget").toggleClass("off");
     $(".mbui-layout-header.layout-header").toggleClass("off");
 }
