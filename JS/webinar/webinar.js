135a6de9c6f79f99db25029383da8e25624a0e50
icardenas <demwl_14@hotmail.com>
2022-03-28 21:35:25 -0300

    files

--- JS/webinar/webinar.js
+++ JS/webinar/webinar.js
@@ -1,7 +1,18 @@
 function estamosaqui(){
     console.log("estamos aquí")
     console.log("Config Webinar 2022 para masterbase")
+    $("#hola").click(testers);
+
+}
+
+function testers(){
+    
+    console.log("holap");
+    $("#control").toggleClass("nolanding");
+    $(".cover").toggleClass("nolanding");
     
 }
 
+
 estamosaqui();
+
